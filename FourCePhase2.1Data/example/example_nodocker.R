3cc130c95e2581e1a58a926c16385d128b997420
ChuanHong <hong.chuan.hannah@gmail.com>
2021-04-14 16:03:30 +0000

    fix namespace

--- FourCePhase2.1Data/example/example_nodocker.R
+++ FourCePhase2.1Data/example/example_nodocker.R
@@ -1,7 +1,7 @@
 rm(list=ls())
-devtools::install_github("https://github.com/covidclinical/Phase2.1SurvivalRPackage", subdir="FourCePhase2.1Survival", upgrade=FALSE, ref="ChuanHong-testing")
+devtools::install_github("https://github.com/covidclinical/Phase2.1DataRPackage", subdir="FourCePhase2.1Data", upgrade=FALSE,ref="ChuanHong-testing")
 library(FourCePhase2.1Data)
-currSiteId = FourCePhase2.1Data::getSiteId()
+currSiteId = "MGB"
 dir.input="/Users/chuanhong/Documents/Input"
 runQC_nodocker(currSiteId, dir.output)
 
